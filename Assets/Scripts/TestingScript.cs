859f90690e6b4d1a4617f2a24ee8340861f8d07f
sinantech <sinancemozer@gmail.com>
2025-02-06 20:15:59 +0300

    First Testing Variables

---
+++ Assets/Scripts/TestingScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+//Variable Types For Unity Engine
+public class TestingScript : MonoBehaviour {
+   int number =9;
+   float anotherNumber = 5.5f;
+   string text = "Hello World";
+   bool isTrue = false;
+
+}
