2e3bbe3bf0e414cc91891ec7782420367853e82a
sinantech <sinancemozer@gmail.com>
2025-02-06 20:56:08 +0300

    Update TestingScript.cs

--- Assets/Scripts/TestingScript.cs
+++ Assets/Scripts/TestingScript.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 //Variable Types For Unity Engine
 public class TestingScript : MonoBehaviour {
-   int number =9;
-   float anotherNumber = 5.5f;
-   string text = "Hello World";
-   bool isTrue = false;
+    
+    private void Start() {
+        
+    }
 
+    private void Update() {
+        
+    }
+
+    void TestFunction() {
+        
+    }
 }
