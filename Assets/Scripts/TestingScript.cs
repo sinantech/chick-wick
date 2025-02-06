b9e08c45d612f2754ba1e118f8cbe30d3176605d
sinantech <sinancemozer@gmail.com>
2025-02-06 21:55:34 +0300

    Update TestingScript.cs

--- Assets/Scripts/TestingScript.cs
+++ Assets/Scripts/TestingScript.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
 
-//Variable Types For Unity Engine
 public class TestingScript : MonoBehaviour {
+
+    // Execute Order of Functions in unity Monobehaviour
+
+    // Executes before game starts and runs once
+    private void Awake() {
+        Debug.Log("Awake");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+         Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update() {
+         Debug.Log("Update");
+    }
+
+    private void FixedUpdate() {
+        
+    }
+
+    private void LateUpdate() {
+        
+    }
     
-    private void Start() {
-        
-    }
-
-    private void Update() {
-        
-    }
-
-    void TestFunction() {
-        
-    }
 }
