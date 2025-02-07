f96b679fdd3d9011331922999d8684b9859ebfc4
sinantech <sinancemozer@gmail.com>
2025-02-07 21:21:35 +0300

    Update TestingScript.cs

--- Assets/Scripts/TestingScript.cs
+++ Assets/Scripts/TestingScript.cs
@@ -1,41 +1,77 @@
 using UnityEngine;
 
-public class TestingScript : MonoBehaviour {
+public class TestingScript : MonoBehaviour
+{
 
     // Execute Order of Functions in unity Monobehaviour
 
     // Executes before game starts and runs once
-    private void Awake() {
+
+    int number = 6;
+    private void Awake()
+    {
         Debug.Log("Awake");
         TestFunction();
-        Debug.LogWarning("");
+        Debug.LogWarning("Warning Log");
+        Debug.LogError("Warning Log");
 
+        switch (number)
+        {
+
+            case 5:
+                Debug.Log("sqjkjsd");
+                break;
+
+            case 7:
+                Debug.Log("p1p1ye");
+                break;
+
+
+        }
+
+
+        if (number > 9)
+        {
+            Debug.Log("s1k ye");
+        }
+        else
+        {
+            Debug.Log("S1k yeme");
+        }
     }
 
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() {
-         Debug.Log("Start");
+    void Start()
+    {
+        Debug.Log("Start");
+
     }
 
     // Update is called once per frame
-    void Update() {
-         Debug.Log("Update");
+    void Update()
+    {
+        Debug.Log("Update");
     }
 
     //Managing Physical Objects and Moves like jumping,hitting etc...
-    private void FixedUpdate() {
-        
+    private void FixedUpdate()
+    {
+
     }
 
     //No necessary we will use fixed update and update 
-    private void LateUpdate() {
-        
+    private void LateUpdate()
+    {
+
     }
 
     // Private functions not execute itself on the engine should be called in the unitys own functions
-    void TestFunction() {
+    void TestFunction()
+    {
         Debug.Log("Test Function");
     }
 
-    
+
 }
