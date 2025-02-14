a65bbc5498aec1baf2b0ed6f6d0dfdc2f6bde5aa
Sinan Can Özer <sinancemozer@gmail.com>
2025-02-15 00:49:00 +0300

    feat:player controller scripts added

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -1,11 +1,36 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    
+    [Header("References")]
     [SerializeField] private Transform _orientationTransform;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float _movementSpeed;
     private Rigidbody _playerRigidbody;
+    private float _horizontalInput, _verticalInput;
+    private Vector3 _movementDirection;
 
     private void Awake() {
         _playerRigidbody = GetComponent<Rigidbody>();
+        _playerRigidbody.freezeRotation = true;
+    }
+
+    private void Update() {
+        setInputs();
+    }
+
+    private void FixedUpdate() {
+        setPlayerMovement();
+    }
+
+    private void setInputs() {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void setPlayerMovement() {
+        _movementDirection = _orientationTransform.forward * _verticalInput + _orientationTransform.right * _horizontalInput;
+        _playerRigidbody.AddForce(_movementDirection * _movementSpeed, ForceMode.Force);
     }
 }
