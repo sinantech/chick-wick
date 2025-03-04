adb62673b3b4b8c38af2547129ce771fc1717d39
Sinan Can Özer <sinancemozer@gmail.com>
2025-03-04 11:33:22 +0300

    v0.28 Sounds On-Off Functionalty Created

--- Assets/_GameAssets/Scripts/UI/SettingsUI.cs
+++ Assets/_GameAssets/Scripts/UI/SettingsUI.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using MaskTransitions;
 using UnityEngine;
@@ -17,10 +18,19 @@ public class SettingsUI : MonoBehaviour
     [SerializeField] private Button _resumeButton;
     [SerializeField] private Button _mainMenuButton;
 
+    [Header("Sprites")]
+    [SerializeField] private Sprite _musicActiveSprite;
+    [SerializeField] private Sprite _musicPassiveSprite;
+    [SerializeField] private Sprite _soundActiveSprite;
+    [SerializeField] private Sprite _soundPassiveSprite;
+
     [Header("Settings")]
     [SerializeField] private float _animationDuration;
 
     private Image _blackBackgroundImage;
+
+    private bool _isMusicActive;
+    private bool _isSoundActive;
 
     void Awake()
     {
@@ -35,6 +45,25 @@ public class SettingsUI : MonoBehaviour
             AudioManager.Instance.Play(SoundType.TransitionSound);
             TransitionManager.Instance.LoadLevel(Consts.SceneNames.MENU_SCENE);
         });
+
+        _musicButton.onClick.AddListener(OnMusicButtonClicked);
+        _soundButton.onClick.AddListener(OnSoundButtonClicked);
+    }
+
+    private void OnSoundButtonClicked()
+    {
+        AudioManager.Instance.Play(SoundType.ButtonClickSound);
+        _isSoundActive = !_isSoundActive;
+        _soundButton.image.sprite = _isSoundActive ? _soundActiveSprite : _soundPassiveSprite;
+        AudioManager.Instance.SetSoundEffectsMute(!_isSoundActive);
+    }
+
+    private void OnMusicButtonClicked()
+    {
+        AudioManager.Instance.Play(SoundType.ButtonClickSound);
+        _isMusicActive = !_isMusicActive;
+        _musicButton.image.sprite = _isMusicActive ? _musicActiveSprite : _musicPassiveSprite;
+        BackgroundMusic.Instance.SetMusicMute(!_isMusicActive);
     }
 
     private void OnSettingsButtonClicked()
